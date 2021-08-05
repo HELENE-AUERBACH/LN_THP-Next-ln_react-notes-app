81a489686ad3a7d7325c584bf9e9bbc071d034e9
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-08-05 19:48:20 +0200

    Initial commit (from "Build a Notes App with React.js (for Beginners) : https://www.youtube.com/watch?v=ulOKYl5sHGk").

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
+import uuid from "react-uuid";
 import './App.css';
+import Sidebar from 'components/Sidebar';
+import Main from 'components/Main';
 
 function App() {
+  const [notes, setNotes] = React.useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+   
+  const [activeNote, setActiveNote] = React.useState(false);
+   
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+   
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "", 
+      lastModified: Date.now(),
+    };
+     
+    setNotes([newNote, ...notes]);
+    setActiveNote(newNote.id);
+  }
+   
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
+      return note;
+    });
+     
+    setNotes(updatedNotesArray);
+  }
+   
+  const onDeleteNote = (noteIdToDelete) => {
+    setNotes(notes.filter(({ id }) => id !== noteIdToDelete));
+  }
+   
+  const getActiveNote = () => {
+    return notes.find(({ id }) => id === activeNote);
+  }
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   );
 }
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,37 @@
+import ReactMarkdown from "react-markdown";
+
+const Sidebar = ({ notes, onAddNote, onDeleteNote, activeNote, setActiveNote }) => {
+  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+   
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button onClick={onAddNote}>Add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        {sortedNotes.map(({ id, title, body, lastModified }) => (
+          <div className={`app-sidebar-note ${id === activeNote && "active"}`}
+            key={id}
+            onClick={() => setActiveNote(id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{title}</strong>
+              <button onClick={(e) => onDeleteNote(id)}>Delete</button>
+            </div>
+            <ReactMarkdown className="markdown-preview">{body && body.substr(0, 100) + "..."}</ReactMarkdown>
+            <small className="note-meta">
+              Last modified{" "}
+              {new Date(lastModified).toLocaleDateString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
